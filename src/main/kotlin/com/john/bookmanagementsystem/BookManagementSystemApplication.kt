9e91a672b28ed2d793cc86f373a3f0f92dd5598e
John Salib <john.salib@nl.abnamro.com>
2023-11-10 17:56:11 +0100

    Initial commit

---
+++ src/main/kotlin/com/john/bookmanagementsystem/BookManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.john.bookmanagementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookManagementSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/john/bookmanagementsystem/BookManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.john.bookmanagementsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagementSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
