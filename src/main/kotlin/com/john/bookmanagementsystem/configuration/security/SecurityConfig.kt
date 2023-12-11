bffc5a710915b35ee8405c781b7f8ee5e32a787a
John Salib <john.salib@nl.abnamro.com>
2023-12-11 12:30:50 +0100

    Replace deprecated security configs with new ones

--- src/main/kotlin/com/john/bookmanagementsystem/configuration/security/SecurityConfig.kt
+++ src/main/kotlin/com/john/bookmanagementsystem/configuration/security/SecurityConfig.kt
@@ -5,30 +5,30 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig constructor(@Autowired private val userDetailsService: CustomUserDetailsService) {
+class SecurityConfig constructor(
+    @Autowired private val userDetailsService: CustomUserDetailsService,
+) {
 
     @Bean
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         httpSecurity
             .csrf().disable()
-            .authorizeHttpRequests()
-            .requestMatchers(HttpMethod.POST, "auth/**").permitAll()
-            .requestMatchers(HttpMethod.GET, "**").permitAll()
-            .requestMatchers(HttpMethod.POST, "author").authenticated()
-            .and()
-            .httpBasic()
+            .authorizeHttpRequests {
+                it.requestMatchers(HttpMethod.POST, "auth/**").permitAll()
+                it.requestMatchers(HttpMethod.GET, "**").permitAll()
+                it.requestMatchers(HttpMethod.POST, "author").authenticated()
+            }
+            .httpBasic(Customizer.withDefaults())
 
         return httpSecurity.build()
     }
