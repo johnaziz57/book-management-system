6c1d893dcb28311484c35c07995d344f84be43de
John Salib <john.salib@nl.abnamro.com>
2023-12-18 11:53:16 +0100

    Extend JWT expiry to 4 hours

--- src/main/kotlin/com/john/bookmanagementsystem/configuration/security/SecurityConstants.kt
+++ src/main/kotlin/com/john/bookmanagementsystem/configuration/security/SecurityConstants.kt
@@ -1,6 +1,5 @@
 package com.john.bookmanagementsystem.configuration.security
 
 object SecurityConstants {
-    const val JWT_EXPIRY = 24 * 60 * 1000
-    const val JWT_KEY = "the_top_shokry_key"
+    const val JWT_EXPIRY = 4 * 60 * 60 * 1000
 }
