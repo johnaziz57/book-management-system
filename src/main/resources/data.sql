a5f6f9914eccc9aaee329bc98476ed832062c513
John Salib <john.salib@nl.abnamro.com>
2023-12-30 19:22:42 +0100

    Add populate the project's database on start up

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO PERSON (id, name, user_name, password, role, borrowed_books_count) VALUES (0, 'user', 'user', '$2a$10$q7iaHFTewlQ9Bs9mmNyrwOUa7g7vWNDvW98ILie/OFc5olt1A5k1S', 0 , 0);
+INSERT INTO PERSON (id, name, user_name, password, role, borrowed_books_count) VALUES (1, 'admin', 'admin', '$2a$10$q7iaHFTewlQ9Bs9mmNyrwOUa7g7vWNDvW98ILie/OFc5olt1A5k1S', 1, 0 );
+INSERT INTO BOOK (id, title, isbn, available_copies) VALUES
+(0, 'The Catcher in the Rye', '9780316769480', 25),
+(1, 'To Kill a Mockingbird', '9780061120084', 20),
+(2, '1984', '9780452284234', 30),
+(3, 'Pride and Prejudice', '9781853260001', 15),
+(4, 'The Great Gatsby', '9780743273565', 18),
+(5, 'One Hundred Years of Solitude', '9780061120024', 22),
+(6, 'The Hobbit', '9780618002214', 27),
+(7, 'Harry Potter and the Sorcerer''s Stone', '9780747532696', 35),
+(8, 'The Lord of the Rings', '9780345339683', 25),
+(9, 'The Da Vinci Code', '9780385504205', 40);
