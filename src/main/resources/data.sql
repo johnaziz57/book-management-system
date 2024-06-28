9d66ce44486dc231344d524b4c22db325d9d04fd
John Salib <john.salib@nl.abnamro.com>
2024-06-28 16:38:49 +0200

    Add flyway migration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
 insert into PERSON (id, name, user_name, password, role, borrowed_books_count) values (0, 'user', 'user', '$2a$10$q7iaHFTewlQ9Bs9mmNyrwOUa7g7vWNDvW98ILie/OFc5olt1A5k1S', 0 , 0);
 insert into PERSON (id, name, user_name, password, role, borrowed_books_count) values (1, 'admin', 'admin', '$2a$10$q7iaHFTewlQ9Bs9mmNyrwOUa7g7vWNDvW98ILie/OFc5olt1A5k1S', 1, 0 );
-insert into BOOK (id, title, isbn, available_copies) values
+insert into BOOK (id, title, isbn, available_copies, published_date)
+values
 (9990, 'The Catcher in the Rye', '978-5-3343-5918-5', 25),
 (9991, 'To Kill a Mockingbird', '978-7-0133-2963-2', 20),
-(9992, '1984', '978-5-5783-5603-2', 30),
+(9992, '1984', '978-5-5783-5603-2', 30, NOW()),
 (9993, 'Pride and Prejudice', '978-4-5799-9285-0', 15),
 (9994, 'The Great Gatsby', '978-5-8444-9484-0', 18),
 (9995, 'One Hundred Years of Solitude', '978-6-5697-9195-5', 22),
